e5c574362568e145a76237cc4b381c3da370dc90
LshMakh <157807032+LshMakh@users.noreply.github.com>
2024-10-01 14:45:40 +0400

    Add files via upload

--- AuthProjWebApi/Controllers/CardsController.cs
+++ AuthProjWebApi/Controllers/CardsController.cs
@@ -1,6 +1,56 @@
-﻿namespace AuthProjWebApi.Controllers
+﻿using AuthProjWebApi.Models;
+using AuthProjWebApi.Packages;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthProjWebApi.Controllers
 {
-    public class CardsController
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+
+    public class CardsController : ControllerBase
     {
+        IPKG_CARD package;
+
+
+        public CardsController(IPKG_CARD package)
+        {
+            this.package = package;
+        }
+
+        List<Card> cards;
+
+        [HttpGet]
+        public IActionResult GetCards() { 
+            
+            List<Card> cards = new List<Card>();
+            try
+            {
+                cards = package.GetCards();
+            }
+            catch (Exception ex) {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+                
+            }
+        return StatusCode(StatusCodes.Status200OK, cards);
+        }
+
+        [HttpPost]
+        public IActionResult SaveCard(Card card)
+        {
+            try
+            {
+                package.SaveCard(card);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+
+            }
+            return StatusCode(StatusCodes.Status200OK);
+        }
+       
     }
 }
